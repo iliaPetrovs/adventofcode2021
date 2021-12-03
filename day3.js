9fb35317f761968dc9fcde66d3c6fdfcad2d6e7c
iliaPetrovs <ilja.petrovs@transferwise.com>
2021-12-03 07:54:33 +0000

    Day 3 second half

--- day3.js
+++ day3.js
@@ -1,10 +1,49 @@
 import fs from "fs";
 import readline from "readline";
 
+// First half
+
+// async function getData() {
+//   let binaryOutput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   let gammaArray = [];
+//   let epsilonArray = [];
+
+//   const fileStream = fs.createReadStream("data/day3.txt");
+
+//   const rl = readline.createInterface({
+//     input: fileStream,
+//   });
+
+//   for await (const line of rl) {
+//     for (let i = 0; i < line.length; i++) {
+//       if (parseInt(line.slice(i, i + 1)) == 1) binaryOutput[i]++;
+//     }
+//   }
+//   binaryOutput.map((item, index) => {
+//     if (item >= 500) {
+//         gammaArray[index] = 1;
+//         epsilonArray[index] = 0;
+//     } else {
+//         gammaArray[index] = 0;
+//         epsilonArray[index] = 1;
+//     }
+//   });
+//   console.log(returnDecimalFromTwoBinaryArrays(gammaArray, epsilonArray));
+// }
+
+// const returnDecimalFromTwoBinaryArrays = (gamma, epsilon) => {
+//     return parseInt(gamma.join(''), 2) * parseInt(epsilon.join(''), 2)
+// }
+
+// getData();
+
+// Second half
+
 async function getData() {
   let binaryOutput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let gammaArray = [];
-  let epsilonArray = [];
+  let initialState = [];
+  let binaryArray = [];
+  const oxygenRating = true;
 
   const fileStream = fs.createReadStream("data/day3.txt");
 
@@ -13,24 +52,61 @@ async function getData() {
   });
 
   for await (const line of rl) {
+    binaryArray.push(line);
+  }
+
+  for (const line of binaryArray) {
     for (let i = 0; i < line.length; i++) {
       if (parseInt(line.slice(i, i + 1)) == 1) binaryOutput[i]++;
     }
   }
+
   binaryOutput.map((item, index) => {
     if (item >= 500) {
-        gammaArray[index] = 1;
-        epsilonArray[index] = 0;
+      initialState[index] = oxygenRating ? 1 : 0;
     } else {
-        gammaArray[index] = 0;
-        epsilonArray[index] = 1;
+      initialState[index] = oxygenRating ? 0 : 1;
     }
   });
-  console.log(returnDecimalFromTwoBinaryArrays(gammaArray, epsilonArray));
+
+  console.log(recursive(binaryArray, initialState, 0, oxygenRating));
 }
 
+const recursive = (binaryArray, initialState, index, oxygenRating) => {
+  if (binaryArray.length == 1) return binaryArray;
+  let newBinaryOutput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let newArray = [];
+  let newState = [];
+
+  for (const line of binaryArray) {
+    if (
+      parseInt(line.slice(index, index + 1)) == parseInt(initialState[index])
+    ) {
+      newArray.push(line);
+    }
+  }
+
+  for (const line of newArray) {
+    for (let i = 0; i < line.length; i++) {
+      if (parseInt(line.slice(i, i + 1)) == 1) {
+        newBinaryOutput[i]++;
+      }
+    }
+  }
+
+  newBinaryOutput.map((item, index) => {
+    if (item >= newArray.length / 2) {
+      newState[index] = oxygenRating ? 1 : 0;
+    } else {
+      newState[index] = oxygenRating ? 0 : 1;
+    }
+  });
+
+  return recursive(newArray, newState, index + 1, oxygenRating);
+};
+
 const returnDecimalFromTwoBinaryArrays = (gamma, epsilon) => {
-    return parseInt(gamma.join(''), 2) * parseInt(epsilon.join(''), 2)
-}
+  return parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
+};
 
 getData();
